1fb493861701d04ff7cdab42f3ad4dcfc6d3da2c
Junpei.Horita <ful-ful-junchan0817@tune.ocn.ne.jp>
2020-12-17 10:23:53 +0000

    サブメニュー検証

--- database/seeds/MenuMasterSeeder.php
+++ database/seeds/MenuMasterSeeder.php
@@ -15,13 +15,24 @@ class MenuMasterSeeder extends Seeder
         DB::table('menu_master')->truncate();
 
         $datas = [
-            ['menu_master_id' => '100', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'ホーム', 'url' => '/home', 'icon' => 'design_app', 'sort' => '10000'],
-            ['menu_master_id' => '200', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Icons', 'url' => '/bootstrap/icons', 'icon' => 'education_atom', 'sort' => '20000'],
-            ['menu_master_id' => '300', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Maps', 'url' => '/bootstrap/map', 'icon' => 'location_map-big', 'sort' => '30000'],
-            ['menu_master_id' => '400', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Notifications', 'url' => '/bootstrap/notifications', 'icon' => 'ui-1_bell-53', 'sort' => '40000'],
-            ['menu_master_id' => '500', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'User Profile', 'url' => '/bootstrap/user', 'icon' => 'users_single-02', 'sort' => '50000'],
-            ['menu_master_id' => '600', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Table List', 'url' => '/bootstrap/tables', 'icon' => 'design_bullet-list-67', 'sort' => '60000'],
-            ['menu_master_id' => '700', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Typography', 'url' => '/bootstrap/typography', 'icon' => 'text_caps-small', 'sort' => '70000'],
+            /**
+             * メインメニュー
+             */
+            ['menu_master_id' => '100', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'ホーム', 'url' => '/home', 'icon' => 'design_app', 'sort' => '100'],
+            ['menu_master_id' => '200', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Icons', 'url' => '/bootstrap/icons', 'icon' => 'education_atom', 'sort' => '200'],
+            ['menu_master_id' => '300', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Maps', 'url' => '/bootstrap/map', 'icon' => 'location_map-big', 'sort' => '300'],
+            ['menu_master_id' => '400', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Notifications', 'url' => '/bootstrap/notifications', 'icon' => 'ui-1_bell-53', 'sort' => '400'],
+            ['menu_master_id' => '500', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'User Profile', 'url' => '/bootstrap/user', 'icon' => 'users_single-02', 'sort' => '500'],
+            ['menu_master_id' => '600', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Table List', 'url' => '/bootstrap/tables', 'icon' => 'design_bullet-list-67', 'sort' => '600'],
+            ['menu_master_id' => '700', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Typography', 'url' => '/bootstrap/typography', 'icon' => 'text_caps-small', 'sort' => '700'],
+
+            ['menu_master_id' => '5000', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 0, 'menu_title' => 'Menu', 'url' => '/menu/MenuList', 'icon' => 'design_bullet-list-67', 'sort' => '5000'],
+
+            /**
+             * サブメニュー
+             */
+            /* メニュー */
+            ['menu_master_id' => '5001', 'menu_type' => MenuModel::MENU_TYPE_PARENT, 'parent_menu_master_id' => 5000, 'menu_title' => 'Menu List', 'url' => '/menu/MenuList', 'icon' => '', 'sort' => '5001'],
         ];
         foreach($datas as $data) {
             DB::table('menu_master')->insert($data);
